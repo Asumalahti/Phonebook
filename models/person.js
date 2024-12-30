4ddb31e985b4532ce125e0e6725ae0b327e82731
Aleksi <aleksi.asumalahti@gmail.com>
2024-12-30 10:22:00 +0200

    env korjaus

--- models/person.js
+++ models/person.js
@@ -5,7 +5,7 @@ if (process.argv.length<3) {
   process.exit(1)
 }
 
-const password = process.argv[2]
+const password = process.env.MONGODB_PASSWORD
 const personName = process.argv[3]
 const personNumber = process.argv[4]
 
